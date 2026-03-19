44983d11dbbfa386278129d31419a196dddd8acb
Leo Collins <collins.leo@proton.me>
2026-03-19 13:53:58 +0000

    Build and publish wheels (#2)
    Based on the template from
    `maturin generate-ci github -m rtree-capi/Cargo.toml`

--- rtree-capi/src/lib.rs
+++ rtree-capi/src/lib.rs
@@ -1,3 +1,13 @@
 mod error;
 mod node;
 mod rtree;
+
+#[cfg(feature = "python")]
+mod python {
+    use pyo3::prelude::*;
+
+    #[pymodule]
+    fn rtree_capi(_py: Python, _m: &Bound<'_, PyModule>) -> PyResult<()> {
+        Ok(())
+    }
+}
